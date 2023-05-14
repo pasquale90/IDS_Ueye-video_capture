d26d385aa525ba9c87e3057e4113aca1a01f3c88
pasquale90 <melissaspaschalis@gmail.com>
2023-05-14 22:21:38 +0300

    implementation for obtaining named arguments

---
+++ ueye/console.h
@@ -0,0 +1,16 @@
+#ifndef CONSOLE_H    // To make sure you don't declare the function more than once by including the header multiple times.
+#define CONSOLE_H
+
+
+
+class InputParser{
+    public:
+        InputParser (int &argc, char **argv);
+        const std::string& getCmdOption(const std::string &option) const;
+        bool cmdOptionExists(const std::string &option) const;
+
+    private:
+        std::vector <std::string> tokens;
+};
+
+#endif
